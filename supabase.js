ab9f8c15f671cf81809268852eb2048f15c00870
raziesrore <mohv13132@gmail.com>
2026-07-13 01:04:07 +0200

    Clarify comments and update Supabase Anon Key
    Updated comments for clarity and replaced the placeholder Anon Key with an actual key.

--- supabase.js
+++ supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-// الرابط الأساسي لمشروعك على Supabase
+// الرابط الأساسي لمشروعك (جاهز ومنظف للربط الصحيح)
 const SUPABASE_URL = 'https://nyxteyfpdautvbmumawi.supabase.co'; 
 
-// مفتاح الأمان العام (Anon Key) الخاص بك
-const SUPABASE_ANON_KEY = 'Customer_Key_Injected_Here';
+// مفتاح الأمان (Anon Key) الخاص بك كاملاً بدون أي نقص
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55eHRleWZwZGF1dHZibXVtYXdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4ODUzMzQsImV4cCI6MjA5OTQ2MTMzNH0.QRCo9yV5PnjqjWPrVopIzHpmMokXLixrX-fyth5A8hE';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
